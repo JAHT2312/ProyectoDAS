82d2b713e71722eeba518a872032a0a8fb224086
Jesus Alonso Huacre Tito <jesushuacre@outlook.com>
2025-07-03 19:35:17 -0500

    Implementacion de Front-Carrito de compras

---
+++ ProyectoDAS/Controllers/CarritoController.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoDAS.Models;
+
+namespace ProyectoDAS.Controllers
+{
+    public class CarritoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
